d711036eba2b9df09bad8b491e24da02f0b90a20
AaronOET <aaronhsu@email.ntou.edu.tw>
2025-03-25 04:02:59 +0800

    refactor shp2ldb and shpbc2pli

--- d3dtools/shpbc2pli.py
+++ d3dtools/shpbc2pli.py
@@ -4,9 +4,8 @@ Convert boundary line shapefile to *.pli file
 This module can be executed using either 'shpbc2pli' or 'shp2pli' command.
 """
 import os
-import glob
-import geopandas as gpd
 import argparse
+from . import utils
 
 
 def convert(input_folder='SHP_BC', output_folder='PLI_BC', id_field=None):
@@ -23,69 +22,27 @@ def convert(input_folder='SHP_BC', output_folder='PLI_BC', id_field=None):
     id_field : str, optional
         Name of the field to use for boundary names. If None, will look for 'ID', 'Id', 'id', or 'iD'
     """
-    # Specify file source
-    fileList = glob.glob(f'{input_folder}/*.shp')
-    print(f"Found {len(fileList)} shapefiles in {input_folder}")
+    # Find and load shapefiles
+    fileList = utils.find_shapefiles(input_folder)
+    gdfs = utils.read_shapefiles(fileList)
 
-    gdfs = []
-    for i, item in enumerate(fileList):
-        gdf = gpd.read_file(item)
-        gdfs.append(gdf)
+    # Extract geometries and boundary names
+    ref_wkts = utils.extract_geometries(gdfs)
+    bcNames = utils.get_boundary_names(gdfs, id_field)
 
-    # Read wkt
-    ref_wkts = []
-    for i, gdf in enumerate(gdfs):
-        ref_wkt = [g.wkt for g in gdf['geometry'].values]
-        ref_wkts.append(ref_wkt)
+    # Create output folder if needed
+    utils.ensure_output_folder(output_folder)
 
-    print(f"Total features: {len(ref_wkts)}")
-
-    # Get boundary names
-    bcNames = []
-    for i, gdf in enumerate(gdfs):
-        if id_field and id_field in gdf.columns: # Check if user-specified field exists
-            # Use the user-specified field name
-            bcName = [name for name in gdf[id_field].values]
-        else:
-            # Check for all possible case variations of the ID field
-            possible_id_fields = ['ID', 'Id', 'id', 'iD']
-            found_id_field = None
-
-            for field in possible_id_fields:
-                if field in gdf.columns:
-                    found_id_field = field
-                    break
-
-            if found_id_field:
-                bcName = [name for name in gdf[found_id_field].values]
-            else:
-                raise KeyError(f"No ID field found in the shapefile. Please ensure your shapefile has one of these fields: {possible_id_fields} or specify the field name using id_field parameter")
-
-        bcNames.append(bcName)
-
-    # Create output folder if not exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-        print(f"Created output folder: {output_folder}")
-
-    # For loop gdfs, create a .pli file with id as its name
+    # Process and write PLI files
     file_count = 0
     for i, ref_wkt in enumerate(ref_wkts):
         for j, item in enumerate(ref_wkt):
-            with open(f'{output_folder}/{bcNames[i][j]}.pli', 'w',
-                    encoding='utf-8') as f:
-                f.write('{}\n'.format(bcNames[i][j]))
-                points = [
-                    point.split() for point in item.replace(
-                        "LINESTRING (", "").replace(")", "").split(',')
-                ]
-                f.write('{} {}\n'.format(len(points), 2))
-                for k, ktem in enumerate(points):
-                    f.write(
-                        f'{float(ktem[0]):.6f} {float(ktem[1]):.6f} {bcNames[i][j]}_{k+1:0>4}\n'
-                    )
-                f.write('\n')
-            file_count += 1
+            boundary_name = bcNames[i][j]
+            points = utils.extract_points_from_wkt(item)
+            filepath = f'{output_folder}/{boundary_name}.pli'
+
+            if utils.write_boundary_file(filepath, boundary_name, points):
+                file_count += 1
 
     print(f'Done! Generated {file_count} PLI files in {output_folder}')
     return file_count
